bf299a6d458b8447dad2e3d90985f4580de8aec5
ikerpacheco <iker.pacheco-figueiredo@epitech.eu>
2023-12-15 13:18:56 +0100

    [ADD] Started main page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,34 @@
+import "./Home.css";
+
 const Home = () => {
   return (
-    <div>
-      <p>HelloWorld</p>
+    <div className="page">
+      <div className="background">
+        <div className="roundBackground" />
+        <div className="content">
+          <div className="leftSide">
+            <div className="infoContainer">
+              <h1 className="titleText">
+                <span className="spanTitle">Studying</span> online is now much
+                easier
+              </h1>
+              <p className="descriptionText">
+                Skilline is an interesting platform that will teach you in more
+                an interactive way
+              </p>
+              <div className="buttonsContainer">
+                <button className="joinFreeButton">Join for free</button>
+                <button className="playButton">play</button>
+                <button className="howItWorksButton">Watch how it works</button>
+              </div>
+            </div>
+          </div>
+          <div className="rightSide"></div>
+        </div>
+      </div>
+      <div className="softwareSectionContainer">
+        <h4 className="trustedText">Trusted by 5000+ Companies Worldwide</h4>
+      </div>
     </div>
   );
 };
